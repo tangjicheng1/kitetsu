4c77d918efa1e69aa2575d5764c3f18d319819b4
tangjicheng <tangjch15@gmail.com>
2024-02-28 15:53:54 +0900

    modified:   kitetsu/client.py new file:   requirements.txt modified:   setup.py

--- kitetsu/client.py
+++ kitetsu/client.py
@@ -2,6 +2,10 @@ import sys
 import quickfix as fix
 import quickfix44 as fix44
 import argparse
+from loguru import logger
+
+log_format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+logger.add(sys.stdout, format=log_format, level="INFO")
 
 class KitetsuClient(fix.Application):
     def onCreate(self, sessionID):
@@ -41,11 +45,14 @@ class KitetsuClient(fix.Application):
 def main():
     parser = argparse.ArgumentParser(description="Kitetsu QuickFIX Client")
     parser.add_argument('-c', '--config', required=True, help='Path to FIX configuration file')
+    parser.add_argument('-l', '--log', required=False, help='Path to log file')
     args = parser.parse_args()
     if 'help' in args:
         parser.print_help()
         return
     config_file = args.config
+    log_file = args.log if args.log else "./kitetsu.log"
+    logger.add(log_file, format=log_format, level="INFO")
 
     try:
         settings = fix.SessionSettings(config_file)
--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='kitetsu',
     version='0.0.1',
     author='tangjicheng',
     author_email='tangjch15@gmail.com',
     packages=find_packages(),
-    install_requires=[
-        'quickfix',  # 确保这里的依赖是正确的，根据QuickFIX Python绑定的实际包名
-    ],
+    install_requires=requirements,
     entry_points={
         'console_scripts': [
             'kitetsu=kitetsu.client:main',
